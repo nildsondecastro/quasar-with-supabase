96ca3ef95e2b24990503f1d742fc9b9c0516f35a
Nildson <nildson@example.com>
2022-08-05 23:34:19 -0300

    ajuste layout da pageMe

--- src/router/routes.js
+++ src/router/routes.js
@@ -7,15 +7,15 @@ const routes = [
       { path: '', name: 'loginDefault', component: () => import('pages/Login.vue') },
       { path: 'Login', name: 'login', component: () => import('pages/Login.vue') },
       { path: 'register', name: 'register', component: () => import('pages/Register.vue') },
-      { path: 'email-confirmation', name: 'email-confirmation', component: () => import('pages/EmailConfirmation.vue') },
-      { path: 'me', name: 'me', component: () => import('pages/Me.vue') }
+      { path: 'email-confirmation', name: 'email-confirmation', component: () => import('pages/EmailConfirmation.vue') }
     ]
   },
   {
     path: '/',
     component: () => import('layouts/MainLayout.vue'),
     children: [
-      { path: '', component: () => import('pages/IndexPage.vue') }
+      { path: 'me', name: 'me', component: () => import('pages/Me.vue') }
+      //{ path: '', component: () => import('pages/IndexPage.vue') }
     ]
   },
 
