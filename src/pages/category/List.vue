8e46024ec9d09e889454d187dc5caa7085271b62
Nildson <nildson@example.com>
2022-08-14 23:18:25 -0300

    platform detection

--- src/pages/category/List.vue
+++ src/pages/category/List.vue
@@ -16,6 +16,8 @@
           <q-space />
 
           <q-btn 
+            v-if="$q.platform.is.desktop"
+            class="desktop-only"
             label="Add New" 
             color="primary"
             icon="mdi-plus"
@@ -40,6 +42,18 @@
         </template>
       </q-table>
     </div>
+    <q-page-sticky 
+      position="bottom-right" 
+      :offset="[18, 18]"
+    >
+      <q-btn 
+        v-if="$q.platform.is.mobile"
+        fab 
+        icon="mdi-plus" 
+        color="primary" 
+        :to="{ name: 'form-category' }"
+      />
+    </q-page-sticky>
   </q-page>
 </template>
 
